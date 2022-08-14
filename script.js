69737f200052009ca567730ebe4d452d2fb41b31
Mercedes Wiesztort <mercedesw13@protonmail.com>
2022-08-14 12:44:38 -0300

    agrega funcionalidad para elegir la cantidad de celdas de la grilla

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
 
 /* 1. agregar al container una cuadrícula de 16x16 cuadrados */
-let amountGrid = 16;
-let grid = amountGrid * amountGrid;
+const gridDefault = 16;
 const container = document.querySelector('#container');
 
-for (let i=0; i < grid; i++) {
-    const elem = document.createElement('div');
-    elem.classList.add('celda');
-    container.appendChild(elem);
-    /* elem.style.backgroundColor = 'red' */
-    elem.addEventListener('mouseenter', () => {
-        elem.style.backgroundColor = 'red';
-    })
-}
+function gridGenerator(gridLength = gridDefault) {
+    console.log(gridLength)
+    let cells = gridLength * gridLength
+    let gridElem = document.getElementById('grid');
+    
+    if (gridElem != null) {
+        gridElem.remove();
+    }
 
+    gridElem = document.createElement('div');
+    gridElem.setAttribute('id','grid');
+    gridElem.style.gridTemplateColumns = "repeat(" + gridLength + ", 1fr)";
+    gridElem.style.gridTemplateRows = "repeat(" + gridLength + ", 1fr)";
+    container.appendChild(gridElem);
+
+    for (let i=0; i < cells; i++) {
+        const elem = document.createElement('div');
+        elem.classList.add('celda');
+        gridElem.appendChild(elem);
+        elem.addEventListener('mouseenter', () => {
+            elem.style.backgroundColor = 'red';
+        })
+    }
+}
 /* Me gustaría hacer un Range input, hay que ver como tomar el valor actual */
 const cells = document.getElementById('number-cells');
-console.log(cells.getAttribute('value'))
+/* console.log(cells.getAttribute('value')) */
 cells.addEventListener('click', () => {
     result = window.prompt('Ingresar la cantidad de celdas de alto de la grilla');
-    amountGrid = result;
-    console.log(result)
-    console.log(amountGrid)
+    let amountGrid = result;
+    gridGenerator(amountGrid);
 })
 
-/* Agregue un botón en la parte superior de la pantalla que enviará al usuario una ventana emergente
-que le preguntará la cantidad de cuadrados por lado para la nueva cuadrícula. */
+gridGenerator();
+
 
 // TODO:
-/* Una vez ingresada, la cuadrícula existente debe eliminarse y debe generarse una nueva cuadrícula */
-/* Debe generarse en el mismo espacio total que antes (por ejemplo, 960 px de ancho) para que tenga un nuevo bloc de dibujo. */
